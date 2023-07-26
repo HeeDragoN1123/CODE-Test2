aeba310d365896a53d836b0b4933bfda3d98e8bf
HeeDragoN1123 <85594242+HeeDragoN1123@users.noreply.github.com>
2023-07-26 20:37:17 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,18 +1,18 @@
+// 문제 해설 ( 오류 너무 지연시간이 오래걸림)
+// 만약 dot[0]이 양수 '이고' dot[1]이 양수라면 1
+//만약 dot[0]이 음수 '이고' dot[1]이 양수라면 2
+//만약 dot[0]이 양수 '이고' dot[1]이 음수라면 3
+//아니라면 4를 표시해주세요 
+
+
 function solution(dot) {
-    if(dot[0] > 0 && dot[1] > 0){
+    if(0 < dot[0] && 0 < dot[1]){
         return 1;
-    }
-    
-    if(dot[0] < 0 && dot[1] > 0){
+    }else if(0 > dot[0] && 0 < dot[1] ){
         return 2;
-    }
-    
-    if(dot[0] < 0 && dot[1] < 0){
+    }else if(0 > dot[0] && 0 > dot[1] ){
         return 3;
-    }
-    
-    if(dot[0] > 0 && dot[1] < 0){
+    }else {
         return 4;
     }
-
 }
