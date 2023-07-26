c2891c3b05accc975f7fc7a8c28e25908476bcd8
HeeDragoN1123 <85594242+HeeDragoN1123@users.noreply.github.com>
2023-07-26 19:23:49 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -9,6 +9,12 @@ function solution(n, k) {
     // let yang = 12000;
     // let drink = 2000;
     
+    // 문제 풀이
+    // let service = Math.trunc(n/10) : 서비스 = 소수점을 없애고 출력 (개수/10);
+    // answer = n*12000 + (k- service)*2000 
+    // 개수(n) * 12000(양꼬치 가격) + (개수(k) - 서비스)*2000(음료가격)
+    
+    
     let service = Math.trunc(n/10)
     
     answer = n*12000 + (k- service)*2000
