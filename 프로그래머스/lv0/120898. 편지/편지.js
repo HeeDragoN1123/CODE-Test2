477ee2a2daa4441db11c17cdab13741effb49391
HeeDragoN1123 <85594242+HeeDragoN1123@users.noreply.github.com>
2023-07-28 18:48:12 +0900

    [level 0] Title: 편지, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.js
@@ -0,0 +1,12 @@
+// 문제해결
+// 할머니가 보기 편하게 쓰기 위해 문자 하나당 가로 값을 2를 할당해야함
+// for문으로 
+
+function solution(message) {
+    var answer = 0;
+    // for (i =0; i<message.length; i ++){
+    //     answer = message.length * 2
+    // }
+    answer = message.length * 2
+    return answer;
+}
