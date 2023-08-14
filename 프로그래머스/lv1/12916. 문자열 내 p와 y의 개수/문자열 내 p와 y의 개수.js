115b6572a4a206cbfda123942cfbf77911e48328
HeeDragoN1123 <85594242+HeeDragoN1123@users.noreply.github.com>
2023-08-14 16:39:39 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 2.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,7 +1,11 @@
+//1. 문자를 대문자 or 소문자로 통일
+//2. for 문으로 s의 글자들을 반복문으로 돌림
+//3. if 문으로 s[i] 글자가 일치하면 숫자를 더해줌
+//4. answer 로 더해진 숫자들의 값이 참인지 거짓인지 구분
+
 function solution(s){
     var answer = true;
 
-    //1. 문자를 대문자 or 소문자로 통일
     s= s.toUpperCase()
     let nump = 0;
     let numy = 0; 
@@ -23,9 +27,6 @@ function solution(s){
     return answer;
 }
 
-var str1 = 'pPoooyY'
-var str2 = 'Pyy'
-solution(str1);
 
 
 
