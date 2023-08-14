e51ebd80ab2d84d6ed76234ecad82b5854fe428e
HeeDragoN1123 <85594242+HeeDragoN1123@users.noreply.github.com>
2023-08-14 17:20:28 +0900

    [level 1] Title: 음양 더하기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,16 +1,6 @@
-
-// function solution(absolutes, signs) {
-//     let answer = 0;
-// 		// 두 배열 길이 같음
-//     for (let i = 0; i < absolutes.length; i++) {
-// 				// 부호에 따라 +-
-//         signs[i] === true ? answer += absolutes[i] : answer -= absolutes[i]
-//     }
-//     return answer;
-// }
-
-
-
+// 1. for문으로 absolutes 안의 배열들을 조건문으로 돌린다
+// 2. signs[i] 가 참이면 ? absolutes를 정답에 더하고 거짓이면 빼는 삼항연산자
+// 3. return answer; 위치가 for 문의 {} 에 들어가면 값이 달라지니 조심
 
 function solution(absolutes, signs) {
     let answer = 0;
