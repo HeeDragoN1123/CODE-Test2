0da656c19dba9cab4de63eac62431c6d2ef74f68
HeeDragoN1123 <85594242+HeeDragoN1123@users.noreply.github.com>
2023-08-19 20:39:43 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,14 +1,20 @@
-// 서울배열의  엘리먼트 중 kim의 위치 x를 찾아 김서방은 x에 있다는 String을 반환하는 함수,
-// index 0 1 이 어서 김서방은 1?
-// includes()?
-// 'kim' index 값을 찾는거 
-// 서울에 특정 인덱스에 kim이 없을떄 반복
-// 인덱스 값은 처음에 0으로 지정해서 하나씩 늘어나도록
-// 인덱스 값을 지정하는 변수 0;
+// 문제해결
+// while 문으로 작성
+// 인덱스를 만들 변수x를 지정
+// while 문으로 seoul[x] 이 'Kim'이 아닐때
+// x++시켜준다
+
+// function solution(seoul) {
+//     let x = 0;
+//     while(seoul[x] !== 'Kim'){
+//         x++
+//     }
+//     return '김서방은 ' + x + '에 있다';
+// }
+
 function solution(seoul) {
-    let x = 0;
-    while(seoul[x] !== 'Kim'){
-        x++
+    for (let i = 0; i < seoul.length; i++) {
+        if (seoul[i] === 'Kim') 
+            return `김서방은 ${i}에 있다`
     }
-    return '김서방은 ' + x + '에 있다';
 }
