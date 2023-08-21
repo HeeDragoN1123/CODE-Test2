8f4fd81f43d2485fc6f54eaba28961f04f5ffba2
HeeDragoN1123 <85594242+HeeDragoN1123@users.noreply.github.com>
2023-08-21 21:31:27 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    var answer = '';
+    
+    if(s.length !==4 && s.length !==6){
+        return false;
+    }
+    
+    for(let i =0; i <s.length ; i++){
+        if(isNaN(s[i])){
+            return false;
+        
+        
+        }
+        
+    }
+    
+    return true;
+}
+
