6c134fcd0197650f98a16af8383477ae7c57bbd9
HeeDragoN1123 <85594242+HeeDragoN1123@users.noreply.github.com>
2023-08-21 22:29:26 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,5 +1,7 @@
+// return 을 써서 앞의 if 결과가 뒤에 영향을 미치지 못하도록해야함
+// return 을 쓰면 함수가 종료되고 결과값이 나와짐
+
 function solution(s) {
-    var answer = '';
     
     if(s.length !==4 && s.length !==6){
         return false;
