57f925a85cb9cb0873cb855de300a2380aea950f
HeeDragoN1123 <85594242+HeeDragoN1123@users.noreply.github.com>
2023-11-14 17:16:00 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+  var answer = [];
+while(n !== 1) {
+  answer.push(n)
+  if(n%2 === 0) {
+      n/=2
+  } else {
+      n = 3 * n + 1
+  }
+}
+
+  answer.push(1)
+  return answer;
+}
+
+console.log(solution(10));
